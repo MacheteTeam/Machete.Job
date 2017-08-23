42b1706359386a3783dd32905e4d50766b1827c0
2411685663@qq.com <2411685663@qq.com>
2017-08-23 13:10:40 +0800

    添加界面、项目依赖

---
+++ machetejob-admin/src/main/java/com/machetejob/MacheteJob.java
@@ -0,0 +1,28 @@
+/**
+ 							The Apache License 2.0
+						 Copyright (c) 2017 TAOLICLOUD 
+		Unless required by applicable law or agreed to in writing, software distributed under
+      the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+      KIND, either express or implied.	See the License for the specific language governing 
+      permissions and limitations under the License. 
+ */
+package com.machetejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 启动类
+ * @author: sky
+ * @version: 
+ * @Datetime:2017年8月23日
+ * @Email: 2411685663@qq.com
+ */
+@SpringBootApplication
+public class MacheteJob {
+    
+   public static void main(String[] args) {
+       SpringApplication.run(MacheteJob.class, args);
+   }
+    
+}
